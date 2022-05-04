38b310750020e0413f8be3dd9afff632e68c6500
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-05-04 14:16:49 +0200

    feat(/js):Added first interface with first data
    I added server_communication.js with a function to put data into the interface (but doesn't get data from server). I added clearDrawing function into sign.js.

--- mutuari-sign/resources/js/main.js
+++ mutuari-sign/resources/js/main.js
@@ -1,8 +1,60 @@
+let dataForSelect = getDataFromServer();
+
+let currentUsersList;
+let currentBorrowList;
+
+const setRightData = (newChosenForm) => {
+    if(newChosenForm == "emprunter") {
+        currentUsersList = []
+        currentBorrowList = []
+    } else if(newChosenForm == "retourner") {
+        currentUsersList = []
+        currentBorrowList = []
+    }
+}
+
+const setBorrowsInSelect = () => {
+    const selectUsers = document.querySelector("#user-select");
+    const selectBorrows = document.querySelector("#borrow-select");
+
+    const currentUser = currentUsersList.find(el => el.userID == selectUsers.value);
+
+    currentUser.borrowsList.forEach(el => {
+        selectBorrows.options[selectBorrows.options.length] = new Option(el.materialName, el.borrowID);
+    })
+}
+
+const setUsersInSelect = () => {
+    //Récupérer l'élément select des utilisateurs.
+    const selectUsers = document.querySelector("#user-select");
+
+    //Enlever toutes les options
+    let lastOpt = selectUsers.lastElementChild; 
+    while (lastOpt) {
+        selectUsers.removeChild(lastOpt);
+        lastOpt = selectUsers.lastElementChild;
+    }
+
+    //Mettre la valeur par défaut
+    selectUsers.options[selectUsers.options.length] = new Option("Prénom et Nom", null)
+
+    currentUsersList.forEach(el => {
+        selectUsers.options[selectUsers.options.length] = new Option(`${el.firstName} ${el.lastName}`, el.userID);
+    });
+}
+
+//Initialisation de l'application
+const initiateApplication = () => {
+    currentUsersList = dataForSelect;
+    setUsersInSelect();
+}
+
 function onWindowClose() {
     Neutralino.app.exit();
 }
 
 Neutralino.init();
+initiateApplication();
 
 Neutralino.events.on("windowClose", onWindowClose);
 
---
+++ mutuari-sign/resources/js/server_communication.js
@@ -0,0 +1,24 @@
+const getDataFromServer = () => {
+    return ([
+        {
+            userID: 0,
+            firstName: "John", 
+            lastName: "Bibi", 
+            borrowsList: [
+                {borrowID: 0, materialName: "Appareil photo Nikon", isBorrowed: false, isReturned: false},
+                {borrowID: 1, materialName: "Caméra Sony", isBorrowed: true, isReturned: false},
+                {borrowID: 2, materialName: "Trépied", isBorrowed: true, isReturned: false},
+            ] 
+        },
+        {
+            userID: 1,
+            firstName: "Béatrice", 
+            lastName: "Sicle", 
+            borrowsList: [
+                {borrowID: 3, materialName: "Appareil photo Sony", isBorrowed: true, isReturned: false},
+                {borrowID: 4, materialName: "Microphone Bird UM1", isBorrowed: false, isReturned: false},
+                {borrowID: 5, materialName: "Perche", isBorrowed: false, isReturned: false},
+            ] 
+        },
+    ])
+}
